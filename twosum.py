eee8e4b51139f3f21e48e59c8048f1c9cb00be22
veldakarimi <veldakarimi@gmail.com>
2020-10-10 16:42:48 +0300

    addition of two sum solution

---
+++ twosum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}
+        ans = []
+        for i in range(len(nums)):
+            secondNumber = target-nums[i]
+            if(secondNumber in dict.keys()):
+                secondIndex = nums.index(secondNumber)
+                if(i != secondIndex):
+                     return sorted([i, secondIndex])
+            dict.update({nums[i]: i})
+                    
