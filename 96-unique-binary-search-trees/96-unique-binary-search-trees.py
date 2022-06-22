d6691f0a51408f551835c35814d08ecb12e9f139
Velda Kiara <32552296+VeldaKiara@users.noreply.github.com>
2022-06-23 02:34:17 +0300

    Time: 28 ms (96.13%), Space: 13.9 MB (12.73%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -1,9 +1,24 @@
 '''
 dynamic programming
 have an initial array with ones, base cases 0 nodes = 1 tree, 1 nodes = 1 tree
+
+create a func,
+all nodes from 1 to n will be considered as root node, but arr start from 0, need a base case
+since 0 nodes -> 1 tree, 1 node -> 1, initialize all nodes to 1's, number trees
+start at 2
+loop through the range from 2, n+1
+total var to store uniques bst
+consider each val as root
+get subtree valuus on the right and left, left -> root - 1, right -> total nodes - root
+
+update total  from the cache numberTrees, 
+return total
+
+
 '''
 class Solution:
     def numTrees(self, n):
+        
         numberTrees = [1] * (n + 1)
         for nodes in range(2, n + 1):
             total= 0
@@ -15,6 +30,7 @@ class Solution:
                 #add to total for each iteration
                 total += numberTrees[left] * numberTrees[right] 
             numberTrees[nodes] = total
+            #return the single node
         return numberTrees[n]
                 
                 
