06900940ec1988e07b024b5b2ffe31908071c877
Velda Kiara <32552296+VeldaKiara@users.noreply.github.com>
2022-06-24 03:11:52 +0300

    Time: 63 ms (28.99%), Space: 15.5 MB (27.97%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -3,12 +3,20 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+'''
+use 2 pointers
+current pointer to 1st node then prev to null
+reverse 1st node then shift pointers to the next nodes
+at the end the prev pointer should point to the head
+'''
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         #create pointers
         prev, curr = None, head
         #shift pointers
         while curr:
+            #temp var for nexts, to be able to update curr.next
             nexts = curr.next
             curr.next = prev
             prev = curr
