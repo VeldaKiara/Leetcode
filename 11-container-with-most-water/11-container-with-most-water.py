631e5e0099a82ac66c2b3e430c95e56171046ab0
Velda Kiara <32552296+VeldaKiara@users.noreply.github.com>
2022-10-03 12:01:32 +0300

    Time: 798 ms (91.27%), Space: 27.5 MB (44.18%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,19 +1,26 @@
+'''
+temporary store values of the container
+have two pointers, right and left, move pointers
+iterate throught the heights to find max area
+area -= len of shorter vertical line * distance between lines
+'''
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        #temporary store values of the container
-        #have two pointers, right and left, move pointers
-        #iterate throught the heights to find max area
-        #area -= len of shorter vertical line * distance between lines
-        current = 0
         left, right = 0, len(height) - 1
-        
+        curr = 0
         while left < right:
-            area = (right - left) * min(height[right],height[left])  
-            current = max(area, current)
+            area = (right - left) * min(height[right], height[left])
+            curr = max(area, curr)
             
             if height[left] < height[right]:
                 left += 1
             else:
                 right -= 1
+        
+        return curr
+
             
-        return current
+            
+            
+            
+        
