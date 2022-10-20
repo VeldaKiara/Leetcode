22ce1f3fb263f47ddc8ce09b59f8d1a75904e523
Velda Kiara <32552296+VeldaKiara@users.noreply.github.com>
2022-10-20 17:46:29 +0300

    Time: 398 ms (98.46%), Space: 14.4 MB (66.46%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -4,9 +4,6 @@ max_count - 1 * (n + 1) + max(tasks)
 """
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        # if n == 0:
-        #     return len(tasks)  
-            
         countss  = list(collections.Counter(tasks).values())
         bun_mx = max(countss)
         output = (bun_mx - 1) * (n + 1) + countss.count(bun_mx)
