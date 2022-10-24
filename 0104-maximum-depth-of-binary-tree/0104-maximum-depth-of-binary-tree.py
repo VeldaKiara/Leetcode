229888111782aabaf3141211ded0ec8ba45f61bf
Velda Kiara <32552296+VeldaKiara@users.noreply.github.com>
2022-10-24 16:16:54 +0300

    Time: 97 ms (12.05%), Space: 15.1 MB (99.92%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -5,19 +5,38 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
+    # recursion
+    # def maxDepth(self, root: Optional[TreeNode]) -> int:
+    #     #DFs, recursive, left + right
+    #     if not root:
+    #         return 0
+    #     return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    #bfs using queues
+     def maxDepth(self, root: Optional[TreeNode]) -> int:
         #DFs, recursive, left + right
         if not root:
             return 0
-        
-        
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
-        # def dfs ( r, l):
-        #     left = left.val
-        #     right = right.val
-        #     while root:
-        #         curr = 0
-        #         max_curr = max(curr, max_curr)
+        curr_level = 0
+        line = deque([root])
+        while line:
+            #go through the queue removing every element in it 
+            for i in range(len(line)):
+                node = line.popleft()
+                if node.left:
+                    line.append(node.left)
+                if node.right:
+                    line.append(node.right)
+            curr_level += 1
+        return curr_level
+      
+                    
+                    
+                    
+                
+            
+    
+    
+       
                 
         
         
